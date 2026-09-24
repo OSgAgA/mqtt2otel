7abf863fe90d7f160011f3390246281ed72b86b8
Guido Roth <guidoroth@hotmail.com>
2026-09-24 22:26:48 +0200

    Enabled open telemetry output for tests.

---
+++ mqtt2otel.ManifestExplorer.Tests/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Testing.Extensions;
+using Microsoft.Testing.Platform.Builder;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace mqtt2otel.ManifestExplorer.Tests
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = await TestApplication.CreateBuilderAsync(args);
+
+            builder.AddOpenTelemetryProvider(
+                withTracing: tracing => tracing
+                    .AddTestingPlatformInstrumentation()
+                    .AddOtlpExporter(),
+                withMetrics: metrics => metrics
+                    .AddTestingPlatformInstrumentation()
+                    .AddOtlpExporter());
+
+            using var app = await builder.BuildAsync();
+
+            await app.RunAsync();
+        }
+    }
+}
